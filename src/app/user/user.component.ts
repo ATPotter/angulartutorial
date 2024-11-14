7f7afe5042124059c1c67971688a05aa09eccf79
Alan Potter <alan.potter@calnexsol.com>
2024-11-14 10:55:21 +0000

    Initial checkin

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { type User } from './user.model';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css' 
+})
+export class UserComponent {
+
+  // Traditional angular approach
+
+  @Input({required: true}) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+
+
+
+  // Signal-based approach
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+
+
+}
